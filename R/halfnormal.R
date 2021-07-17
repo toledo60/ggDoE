efa6bdbfc22411ca86664bcddfa81ed00352da6d
Jose Toledo <toledojose60@gmail.com>
2021-07-17 13:14:54 -0700

    improved halfnormal() function with more features. Includes new reference lines, show margin of errors based on unrepx PSE,ME,SME

--- R/halfnormal.R
+++ R/halfnormal.R
@@ -1,60 +1,102 @@
 #' Half-Normal Effects Plots
 #'
 #' @param obj object of class lm
-#' @param alpha significance level for the Lenth method. Default is 0.05
-#' @param signif_label If TRUE, only the significant factors according to the Lenth method
-#' (significance level given by alpha) are labeled
-#'
-#' @return A dataframe with the absolute effects and half-normal qunatiles.
-#' A ggplot version of halfnormal plot for factorial effects is returned
-#' @importFrom ggplot2 ggplot aes geom_point geom_text theme_classic labs
-#' @importFrom stats qnorm
+#' @param method Character value. Method to calculate PSE. Default is Lenth. Options include: Zahn, WZahn, Lenth, RMS, Dong, JuanPena, Daniel
+#' @param alpha specify the significance level. Default is alpha=0.05
+#' @param label_active If TRUE, active effects are labeled if the effects cross the computed margin of error (ME). See method argument for more details
+#' @param ref_line Dafault is TRUE, if FALSE the abline with slope (1/PSE) is not displayed. Reference line should follow along most points that are not considered outliers.
+#' @param showME Default is FALSE, if TRUE the cutoffs for margin of errors (ME) and simultaneous margin of error (SME) are shown
+#' @param showplot Default is TRUE, if FALSE plot will not be shown and a tibble is returned used to create the plot along with the calculated PSE,ME,SME
+#' @return A tibble with the absolute effects and half-normal quantiles. A ggplot2 version of halfnormal plot for factorial effects is returned
+#' @importFrom ggplot2 ggplot aes geom_point geom_text theme_bw labs geom_vline annotate geom_abline element_blank
+#' @importFrom stats qnorm coef
 #' @importFrom utils tail
+#' @importFrom unrepx PSE ME
+#' @importFrom tibble tibble
 #' @export
 #'
 #' @examples m1 <- lm(lns2 ~ (A+B+C+D)^4,data=original_epitaxial)
 #' halfnormal(m1)
-#' halfnormal(m1,alpha=0.1)
-#' halfnormal(m1,alpha=0.2,signif_label=TRUE)
-halfnormal <- function(obj,alpha=0.05,signif_label=FALSE){
+#' halfnormal(m1,alpha=0.1,label_active=TRUE,showME=TRUE)
+#' halfnormal(m1,method='Zahn',alpha=0.1,ref_line=TRUE,label_active=TRUE,showME=TRUE)
+halfnormal <- function(obj,method='Lenth',
+                       alpha=0.05,
+                       label_active=FALSE,
+                       ref_line = FALSE,
+                       showME = FALSE,
+                       showplot=TRUE){
   if (inherits(obj, "lm")) {
     i <- pmatch("(Intercept)", names(coef(obj)))
     if (!is.na(i))
       effects <- coef(obj)[-pmatch("(Intercept)", names(coef(obj)))]
     obj <- 2 * effects
   }
-  b <- obj
-  m <- length(b)
-  d <- m/3
-  s0 <- 1.5 * median(abs(b))
-  cj <- as.numeric(b[abs(b) < 2.5 * s0])
-  PSE <- 1.5 * median(abs(cj))
-  ME <- qt(1 - alpha/2, d) * PSE
+  estimates <- obj
+  PSE <- unrepx::PSE(estimates,method = method)
+  ME <- unrepx::ME(estimates,method = method,alpha = alpha)[1]
+  SME <- unrepx::ME(estimates,method = method,alpha=alpha)[2]
 
-  effs <- sort(abs(obj))
+  effs <- sort(abs(estimates))
   names <- names(effs)
+  m <- length(estimates)
   r <- c(1:m)
   zscore<-c(rep(0,m))
+
   for (i in 1:m) {
     zscore[i]<-qnorm( ( ( r[i]-.5)/m+1)/2 )
-    if(signif_label){
+    if(label_active){
       logc<-(abs(effs[i])<= ME)
-      if (logc) {names[i]<-" "}}
+      if (logc) {names[i]<-NA}}
   }
 
-  dat <- tibble("effects"=names,
-                "absolute_effects"=effs,
-                "half_normal_quantiles"=zscore
+  dat <- tibble::tibble("effects"=names,
+                        "absolute_effects"=effs,
+                        "half_normal_quantiles"=zscore
   )
 
+  if(showplot){
+    base_plot <- ggplot(dat,aes_string(x= 'absolute_effects',
+                                       y = 'half_normal_quantiles',
+                                       label='effects')) +
+      geom_point(color = "#1b9e77", size = 2.5)+
+      geom_text(hjust = 0,nudge_x = 0.01*max(dat$absolute_effects),
+                check_overlap = TRUE,na.rm = TRUE)+
+      theme_bw()+
+      theme(panel.grid.major = element_blank(), panel.grid.minor = element_blank())+
+      labs(x="absolute effects",y="half-normal quantiles")
 
-  plot <- ggplot(dat,aes_string(x= 'absolute_effects',
-                         y = 'half_normal_quantiles',
-                         label='effects')) +
-    geom_point(color = "#1b9e77", size = 2)+
-    geom_text(hjust = 0,nudge_x = 0.01*max(dat$absolute_effects),check_overlap = TRUE)+
-    theme_classic()+
-    labs(x="absolute effects",y="half-normal quantiles")
-
-  return(list(dat=dat,plot=plot))
+    if(ref_line & showME){
+      plt <- base_plot + geom_abline(intercept=0,slope = 1/PSE,linetype=2)+
+        geom_vline(xintercept = ME,linetype=3)+
+        annotate("text",x=ME,y=-Inf,hjust=-0.2,vjust=-0.5,
+                 label="ME",fontface="italic",size=2.8)+
+        geom_vline(xintercept=SME,linetype=3)+
+        annotate("text",x=SME,y=-Inf,hjust=-0.2,vjust=-0.5,
+                 label="SME",fontface="italic",size=2.8)+
+        labs(x="absolute effects",y="half-normal quantiles",
+             caption = paste0(method," ME=",round(ME,2),",",
+                              " SME=",round(SME,2)))
+    }
+    else if (!ref_line & !showME){
+      plt <- base_plot
+    }
+    else if(!ref_line & showME){
+      plt <- base_plot +geom_vline(xintercept = ME,linetype=3)+
+        annotate("text",x=ME,y=-Inf,hjust=-0.2,vjust=-0.5,
+                 label="ME",fontface="italic",size=2.8)+
+        geom_vline(xintercept=SME,linetype=3)+
+        annotate("text",x=SME,y=-Inf,hjust=-0.2,vjust=-0.5,
+                 label="SME",fontface="italic",size=2.8)+
+        labs(x="absolute effects",y="half-normal quantiles",
+             caption = paste0(method," ME=",round(ME,2),",",
+                              " SME=",round(SME,2)))
+    }
+    else if(ref_line & !showME){
+      plt <- base_plot + geom_abline(intercept=0,slope = 1/PSE,linetype=2)
+    }
+    return(plt)
+  }
+  else{
+    return(dat)
+  }
 }
